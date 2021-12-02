6362368b8534e9eadd95e9c8dd462c35f6da7a33
Aaron Stanley King <aaron.king@progress.com>
2021-12-02 14:02:01 -0500

    code and stuff

---
+++ Program.fs
@@ -0,0 +1,15 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+printfn "Console App: Started!"
+
+open System.IO
+let baseDirectory = __SOURCE_DIRECTORY__
+let filePath = "Text.txt"
+let fullPath = Path.Combine(baseDirectory, filePath)
+
+let readFilesTask (path1) =
+   task {
+        let! content = File.ReadAllTextAsync(path1)
+        return content
+   }
+
+printfn "Content: %s" (readFilesTask(fullPath).Result)
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FsharpConsoleApp
 Just playing around... inspired by https://www.twitch.tv/laylacodesit 
+
+It does this magic output:
+
+`Console App: Started!` 
+
+`Content: Hello World!`
